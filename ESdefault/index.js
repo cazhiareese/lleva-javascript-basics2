4ec88777a9f3df75323833a1f25b5268085957af
cazhiareese <clleva@up.edu.ph>
2024-03-05 21:51:59 +0800

    add js prelab folders

---
+++ ESdefault/index.js
@@ -0,0 +1,4 @@
+import math from './math.js'
+// the import can be named anything
+
+console.log(math(5, 2)); // note that we referred to it as “math” since that was the name used in the import. But it can be anything
---
+++ commonJS/index.js
@@ -0,0 +1,8 @@
+console.log(module)
+
+
+const add = require('./math.js');
+console.log(module)
+// the import can be named anything
+
+console.log(add(5, 2)); // 7
